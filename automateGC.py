435580d31708a5c62ef781e9d37b4a9a3acb5f2b
Swochchho <70541394+Swochchho@users.noreply.github.com>
2021-10-10 09:02:20 +0600

    Update automateGC.py

--- automateGC.py
+++ automateGC.py
@@ -1,10 +1,10 @@
 import requests
-resp = requests.get("https://api.telegram.org/bot2020906583:AAEU3u405rf6MwrF_blHQbkFTaUlM3DbCdU/getUpdates")
+resp = requests.get("https://api.telegram.org//getUpdates")
 
 #to send messages
-base_url = "https://api.telegram.org/bot2020906583:AAEU3u405rf6MwrF_blHQbkFTaUlM3DbCdU/sendMessage"
+base_url = "https://api.telegram.org//sendMessage"
 parameters = {
-    "chat_id" : "-507611847",
+    "chat_id" : "",
     "text" :input ("Enter Your TEXT here: ")  
 }
 
